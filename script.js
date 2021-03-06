10288aee84bb9a54a3634e3e83317a935ddc282e
Oscar Dominguez <dominguez.celada@gmail.com>
2021-03-06 23:04:46 +0100

    feat: initial version

---
+++ script.js
@@ -0,0 +1,7 @@
+/**
+ * An octoherd script to watch, unwatch or ignore a repository
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
